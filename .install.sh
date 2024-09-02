ea6227b1c88f184586c36c61f389915f3edff809
1u5t1n14n <justus.forstbauer@proton.me>
2024-09-02 16:27:30 +0200

    Update Installation Script (for my personal use)

--- .install.sh
+++ .install.sh
@@ -89,7 +89,7 @@ sudo rm -rf yay/
 pacman -S pipewire wireplumber pipewire-audio pipewire-alsa hyprland btop inkscape krita libreoffice obsidian rofi-wayland zsh wezterm
 systemctl --user enable pipewire
 chsh -s /usr/bin/zsh
-yay -S hyprpaper zen-browser-bin superfile
+yay -S hyprpaper zen-browser-bin superfile zip unzip ttf-jetbrains-mono-nerd omm jetbrains-toolbox
 exit
 exit
 umount -a
