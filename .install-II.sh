7d52cfda2b91f1f8b6762024dca877a79dda73da
1u5t1n14n <justus.forstbauer@proton.me>
2024-09-03 16:42:37 +0200

    Update .install-II.sh

--- .install-II.sh
+++ .install-II.sh
@@ -9,7 +9,7 @@ passwd "${user}"
 pacman -S base-devel git dosfstools grub efibootmgr lvm2 mtools neovim libpulse networkmanager sudo ly
 pacman -S linux linux-headers linux-lts linux-lts-headers
 pacman -S linux-firmware
-pacman -S mesa libva-mesa-driver intel-media-driver
+pacman -S --needed mesa libva-mesa-driver intel-media-driver
 
 sed -i '/en_US.UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
@@ -38,18 +38,6 @@ sed -i '/Color/s/^#//g' /etc/pacman.conf
 sed -i '/ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
 sed -i "38i\ILoveCandy" /etc/pacman.conf
 
-su "${user}"
-cd || exit
-git clone https://aur.archlinux.org/yay.git
-cd yay/ || exit
-makepkg -si
-cd || exit
-sudo rm -rf yay/
-sudo pacman -S pipewire wireplumber pipewire-audio pipewire-alsa hyprland btop inkscape krita libreoffice obsidian rofi-wayland zsh wezterm
-systemctl --user enable pipewire
-chsh -s /usr/bin/zsh
-yay -S hyprpaper zen-browser-bin superfile zip unzip ttf-jetbrains-mono-nerd omm jetbrains-toolbox
-exit
 echo "do the following."
 echo "exit"
 echo "umount -a"
