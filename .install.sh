b946358c39b2fdc5ecea24e0f9ca64fbf876fd43
1u5t1n14n <justus.forstbauer@proton.me>
2024-09-05 14:00:30 +0200

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -12,15 +12,14 @@ read home
 sfdisk /dev/"${disk}" <<EOF
 label: gpt
 size=1G, type=uefi
-size=1M, type=bios
 size=16G, type=linux-swap
 size=0, type=lvm
 EOF
 
 mkfs.fat -F32 /dev/"${disk}"1
-cryptsetup luksFormat /dev/"${disk}"4
+cryptsetup luksFormat /dev/"${disk}"3
 # enter password
-cryptsetup open --type luks /dev/"${disk}"4 lvm
+cryptsetup open --type luks /dev/"${disk}"3 lvm
 # enter password
 pvcreate /dev/mapper/lvm
 vgcreate "${device}" /dev/mapper/lvm
@@ -32,13 +31,13 @@ vgchange -ay
 
 mkfs.ext4 /dev/"${device}"/lv_root
 mkfs.ext4 /dev/"${device}"/lv_home
-mkswap /dev/"${disk}"3
+mkswap /dev/"${disk}"2
 mount /dev/"${device}"/lv_root /mnt
 mkdir /mnt/boot
 mount /dev/"${disk}"1 /mnt/boot
 mkdir /mnt/home
 mount /dev/"${device}"/lv_home /mnt/home
-swapon /dev/"${disk}"3
+swapon /dev/"${disk}"2
 pacstrap -i /mnt base
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
