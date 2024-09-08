80ee36fbdece72f4e6e1e4cf8ee0eeb95aaca7a0
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2024-09-08 17:01:04 +0200

    Update .install-usr.sh

--- .install-usr.sh
+++ .install-usr.sh
@@ -9,13 +9,14 @@ sudo rm -rf yay/
 sudo pacman -S pipewire wireplumber pipewire-audio pipewire-alsa hyprland btop inkscape krita libreoffice obsidian rofi-wayland zsh wezterm
 systemctl --user enable pipewire
 chsh -s /usr/bin/zsh
-yay -S hyprpaper zen-browser-bin superfile zip unzip ttf-jetbrains-mono-nerd omm jetbrains-toolbox
-rm -rf ~/*
-rm -rf ~/.* 2> /dev/null
+yay -S hyprpaper zen-browser-bin superfile zip unzip ttf-jetbrains-mono-nerd omm display-link jetbrains-toolbox
+sudo system TL enable display-link.service
+sudo rm -rf ~/*
+sudo rm -rf ~/.* 2> /dev/null
 git clone https://github.com/1u5t1n14n/.dotfiles ~/
-exit
 echo "do the following:"
 echo "exit"
 echo "umount -a"
 echo "swapoff -a"
 echo "reboot"
+exit
